0441c8f7d7bc411327e864e9dfb248f665f0d1f2
dannasessha <sessha@zingolabs.com>
2022-04-29 13:29:33 +0000

    refactor selftests with `script` wrapper, clean build

--- tests/checktests.rs
+++ tests/checktests.rs
@@ -2,28 +2,39 @@ mod common;
 use std::str;
 
 #[test]
+#[cfg_attr(not(feature = "selftest"), ignore)]
 fn attempt_generic_output() {
-    assert!(common::attempt_output().status.success())
+    let out = common::attempt_output();
+    assert!(out.status.success());
+    let outstr = str::from_utf8(&out.stdout).unwrap();
+    assert!(outstr.contains("testing"));
+    assert!(outstr.contains("Script done."));
 }
 
 #[test]
+#[cfg_attr(not(feature = "selftest"), ignore)]
 fn checkmate_help_output_contains_client_name_and_description() {
-    let out = common::checkmate_help_output().stdout;
-    assert!(str::from_utf8(&out).unwrap().contains("cargo-checkmate"));
-    assert!(str::from_utf8(&out).unwrap().contains(
+    common::clean_build_checkmate();
+    let s_out = common::checkmate_help_output().stdout;
+    let outstr = str::from_utf8(&s_out).unwrap();
+    assert!(outstr.contains("cargo-checkmate"));
+    assert!(outstr.contains(
         "checkmate checks all the things - comprehensive out-of-the-box safety & hygiene checks."
     ));
 }
 
 #[test]
+#[cfg_attr(not(feature = "selftest"), ignore)]
 fn checkmate_output_status_successful() {
+    common::clean_build_checkmate();
     assert!(common::checkmate_output().status.success())
 }
 
 #[test]
+#[cfg_attr(not(feature = "selftest"), ignore)]
 fn checkmate_passes_checkmate() {
-    let out = common::checkmate_output().stdout;
-    assert!(!str::from_utf8(&out)
-        .unwrap()
-        .contains("cargo-checkmate result: FAILED."));
+    common::clean_build_checkmate();
+    let s_out = common::checkmate_output().stdout;
+    let outstr = str::from_utf8(&s_out).unwrap();
+    assert_eq!(false, outstr.contains("cargo-checkmate result: FAILED."));
 }
--- tests/common.rs
+++ tests/common.rs
@@ -1,25 +1,39 @@
 use std::process::Command;
 use std::process::Output;
 
+pub fn clean_build_checkmate() {
+    Command::new("cargo")
+        .arg("clean")
+        .arg("-p")
+        .arg("cargo-checkmate")
+        .status()
+        .expect("cargo clean failed");
+    Command::new("cargo")
+        .arg("build")
+        .arg("-q")
+        .status()
+        .expect("cargo build failed");
+}
+
+pub fn attempt_output() -> Output {
+    let cli_arg = "echo testing";
+    assemble_command(cli_arg)
+}
+pub fn checkmate_help_output() -> Output {
+    let cli_arg = "./target/debug/cargo-checkmate --help";
+    assemble_command(cli_arg)
+}
+pub fn checkmate_output() -> Output {
+    let cli_arg = "./target/debug/cargo-checkmate";
+    assemble_command(cli_arg)
+}
+
 pub fn assemble_command(argument: &str) -> Output {
-    Command::new("sh")
+    Command::new("script")
+        .arg("-O")
+        .arg("selftest_stdout")
         .arg("-c")
         .arg(argument)
         .output()
         .expect("failed to execute")
-}
-
-pub fn attempt_output() -> Output {
-    let arg = "echo testing";
-    assemble_command(arg)
-}
-
-pub fn checkmate_help_output() -> Output {
-    let arg = "./target/debug/cargo-checkmate --help";
-    assemble_command(arg)
-}
-
-pub fn checkmate_output() -> Output {
-    let arg = "./target/debug/cargo-checkmate";
-    assemble_command(arg)
 }
