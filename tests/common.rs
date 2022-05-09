39d973bac5b03c1772148f9f374b171de3c001dc
dannasessha <sessha@zingolabs.com>
2022-05-09 20:29:16 +0000

    remove script usage, refine integration tests

--- tests/common.rs
+++ tests/common.rs
@@ -1,19 +1,9 @@
 use std::process::Command;
 use std::process::Output;
 
-pub fn check_status_command(argument: &str) -> Output {
-    Command::new("cargo-checkmate")
+pub fn checkmate_command(argument: &str) -> Output {
+    Command::new("./target/debug/cargo-checkmate")
         .arg(argument)
         .output()
         .expect("command status with args failed")
-}
-
-pub fn assemble_command(argument: &str) -> Output {
-    let mut exe: String = String::from("./target/debug/cargo-checkmate ");
-    exe.push_str(argument);
-    Command::new("script")
-        .arg("-c")
-        .arg(&exe)
-        .output()
-        .expect("failed to execute")
 }
--- tests/selftests.rs
+++ tests/selftests.rs
@@ -4,9 +4,9 @@ mod selftests {
     use std::str;
 
     #[test]
-    fn checkmate_help_output_contains_client_name_and_description() {
-        let s_out = super::common::assemble_command("--help").stdout;
-        let outstr = str::from_utf8(&s_out).unwrap();
+    fn checkmate_dashdash_help_output_contains_client_name_and_description() {
+        let s_out = super::common::checkmate_command("--help").stdout;
+        let outstr = dbg!(str::from_utf8(&s_out).unwrap());
         assert!(outstr.contains("cargo-checkmate"));
         assert!(outstr.contains(
         "checkmate checks all the things - comprehensive out-of-the-box safety & hygiene checks."
@@ -14,52 +14,83 @@ mod selftests {
     }
 
     #[test]
-    fn checkmate_hook_output_contains_client_name_and_description() {
-        let s_out = super::common::assemble_command("hook").stdout;
+    fn checkmate_help_output_contains_client_name_and_description() {
+        let s_out = super::common::checkmate_command("help").stdout;
+        let outstr = dbg!(str::from_utf8(&s_out).unwrap());
+        assert!(outstr.contains("cargo-checkmate"));
+        assert!(outstr.contains(
+        "checkmate checks all the things - comprehensive out-of-the-box safety & hygiene checks."
+    ));
+    }
+
+    #[test]
+    fn checkmate_format_status_successful() {
+        assert!(super::common::checkmate_command("format").status.success())
+    }
+
+    #[test]
+    fn checkmate_format_stdout_empty() {
+        let s_out = super::common::checkmate_command("format").stdout;
         let outstr = str::from_utf8(&s_out).unwrap();
-        assert!(outstr.contains("cargo-checkmate-hook"));
-        assert!(outstr.contains("manage repository hooks"));
-        assert!(outstr.contains("cargo-checkmate hook <SUBCOMMAND>"));
+        assert!(outstr.is_empty());
     }
 
     #[test]
-    fn checkmate_format_output_status_successful() {
-        assert!(super::common::check_status_command("format")
-            .status
-            .success())
+    fn checkmate_check_status_successful() {
+        assert!(super::common::checkmate_command("check").status.success())
     }
 
     #[test]
-    fn checkmate_check_output_status_successful() {
-        assert!(super::common::check_status_command("check")
-            .status
-            .success())
+    fn checkmate_clippy_status_successful() {
+        assert!(super::common::checkmate_command("clippy").status.success())
     }
 
     #[test]
-    fn checkmate_clippy_output_status_successful() {
-        assert!(super::common::check_status_command("clippy")
-            .status
-            .success())
+    fn checkmate_doc_status_successful() {
+        assert!(super::common::checkmate_command("doc").status.success())
     }
 
     #[test]
-    fn checkmate_doc_output_status_successful() {
-        assert!(super::common::check_status_command("doc").status.success())
+    fn checkmate_build_status_successful() {
+        assert!(super::common::checkmate_command("build").status.success())
     }
 
     #[test]
-    fn checkmate_build_output_status_successful() {
-        assert!(super::common::check_status_command("build")
-            .status
-            .success())
-    }
-
-    #[test]
-    fn checkmate_foo_output_status_fails() {
+    fn checkmate_foo_status_fails() {
         assert_eq!(
             false,
-            super::common::check_status_command("foo").status.success()
+            super::common::checkmate_command("foo").status.success()
         )
+    }
+
+    #[test]
+    fn checkmate_foo_stderr_contains_specific_response() {
+        let s_err = super::common::checkmate_command("foo").stderr;
+        let err = str::from_utf8(&s_err).unwrap();
+        assert_eq!(
+            true,
+            err.contains(
+                "error: Found argument 'foo' which wasn't expected, or isn't valid in this context"
+            )
+        );
+        assert_eq!(true, err.contains("USAGE:"));
+        assert_eq!(true, err.contains("cargo-checkmate [SUBCOMMAND]"));
+    }
+
+    #[test]
+    fn checkmate_hook_invocation_with_no_arguments_fails() {
+        let out = super::common::checkmate_command("hook");
+        // debug shows out.status = ExitStatus(unix_wait_status(256,),)
+        assert_eq!(false, out.status.success());
+    }
+
+    // using `hook` the help output is printed to TTY with stderr
+    #[test]
+    fn checkmate_hook_invocation_with_no_arguments_stderr_contains_client_name_and_description() {
+        let s_err = super::common::checkmate_command("hook").stderr;
+        let outerr = str::from_utf8(&s_err).unwrap();
+        assert!(outerr.contains("cargo-checkmate-hook"));
+        assert!(outerr.contains("manage repository hooks"));
+        assert!(outerr.contains("cargo-checkmate hook <SUBCOMMAND>"));
     }
 }
